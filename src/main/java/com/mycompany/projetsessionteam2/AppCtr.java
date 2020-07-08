b4070b1ae45f6e7a4ab5e2b1013d81dbcce02f34
RB <RB@LAPTOP-8EB5BJOK>
2020-07-07 22:37:50 -0400

    commit_refactoring8

--- src/main/java/com/mycompany/projetsessionteam2/AppCtr.java
+++ src/main/java/com/mycompany/projetsessionteam2/AppCtr.java
@@ -7,18 +7,17 @@ public class AppCtr {
 
     public static void main(String[] args) throws IOException {
       
-       //Creer le JSONObject terrain
+       //Construire le JSONObject terrain
         JSONObject monTerrain = Terrain.creerTerrain();
-        System.out.println(monTerrain.toString()); 
+       
         //Creer le fichier d'entree
         Utilitaire.saveJsonIntoFile(monTerrain.toString(), "c:\\temp\\"+args[0]);
         
        //Traitement : creer le JSONObject de sortie
         String json = Utilitaire.loadJsonIntoString("c:\\temp\\"+args[0]);
-        
         JSONObject terrain  = JSONObject.fromObject(json);
         JSONObject sortie = Utilitaire.retournerSortie(terrain);
-        System.out.println(sortie);
+        
        
         //Creer le fichier de sortie
         Utilitaire.saveJsonIntoFile(sortie.toString(), "c:\\temp\\"+args[1]);
